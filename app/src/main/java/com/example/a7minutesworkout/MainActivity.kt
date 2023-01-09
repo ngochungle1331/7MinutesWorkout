177150a3f047e68a678a6f6430f126cdc1825793
Ngoc Hưng Lê <ngochungle1331@gmail.com>
2023-01-09 23:10:34 +0700

    add BMI screen

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -20,6 +20,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ExerciseActivity::class.java)
             startActivity(intent)
         }
+
+        binding?.flBIM?.setOnClickListener {
+            val intent = Intent(this, BMIActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroy() {
